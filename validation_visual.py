75c92b45d39b9eeae2d201f52e138e4133ffdc62
Robin Gartz <robingartz1@gmail.com>
2024-03-17 21:30:10 +0800

    Run last_model fixes

--- validation_visual.py
+++ validation_visual.py
@@ -7,24 +7,32 @@ class Run:
     def __init__(self):
         pass
 
-    def run_with_model(self, last=False):
-        last_model_name = ""
-        if last:
+    def run_with_model(self, last_model_name=""):
+        last = False
+        if last_model_name == "" or last_model_name == "last":
+            last = True
             with open("models/.last_model_name.txt", 'r') as f:
-                last_model_name = f.read()
-            #last_model_name += ".zip"
+                last_model_names = f.readlines()
         sims = 190_000
         total_time_steps = 160_000
 
-        env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0", max_episode_steps=240*12, render_mode="human", verbose=True)
+        env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0", max_episode_steps=240*5, render_mode="human",
+                       verbose=True, save_verbose=False)
 
         # load models
         model = None
         if last:
-            print("Loading model:", last_model_name)
-            model = PPO.load("models/"+last_model_name, env)
+            last_model_names.reverse()
+            for last_model_name in last_model_names:
+                print("Loading model:", last_model_name)
+                last_model_name = last_model_name.strip('\n')
+                try:
+                    model = PPO.load(last_model_name, env)
+                    break
+                except:
+                    pass
         else:
-            model = PPO.load("models/PPO-v50k-R2-240207_190220", env)
+            model = PPO.load("models/PPO-v50k-R2-240207_190923", env)
             #model = SAC.load("models/R3.1-vary-lr_ground/SAC-v55k-R2-1697792186", env)
             #model = SAC.load("models/old/R3.0-vary-lr_ground/SAC-v55k-R2-1697734035", env)
             #model = A2C.load("models/R3-vary-lr/A2C-v750k-R2-1697750396", env)
@@ -43,11 +51,11 @@ class Run:
             #   obs = vec_env.reset()
 
     def run_without_model(self):
-        sims = 5
-        steps_per_sim = 240 * 4
+        sims = 18
+        steps_per_sim = 240 * 1
         env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0", max_episode_steps=steps_per_sim, render_mode="human")
 
-        observation, info = env.reset(seed=42)
+        observation, info = env.reset()#seed=42)
         for sim in range(sims):
             observation, info = env.reset()
             for step in range(steps_per_sim):
@@ -61,5 +69,5 @@ class Run:
 
 
 if __name__ == '__main__':
-    Run().run_with_model(last=True)
+    Run().run_with_model()
     #Run().run_without_model()
