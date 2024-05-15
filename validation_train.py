a280b3877700a60e4448721d6dec0b0a8a9a3ad0
Robin Gartz <76932159+robingartz@users.noreply.github.com>
2024-05-15 14:17:26 +0800

    Fix ep_step_limit bug

--- validation_train.py
+++ validation_train.py
@@ -12,7 +12,7 @@ class Run:
         self.total_time_steps = total_time_steps
         file_name_append = "A3"
 
-        env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0", max_episode_steps=240*2, verbose=True, total_steps_limit=self.total_time_steps)
+        env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0", ep_step_limit=240*2, verbose=True, total_steps_limit=self.total_time_steps)
 
         # create new models
         #model = PPO("MultiInputPolicy", env, n_steps=20000, batch_size=128, n_epochs=20, verbose=1, learning_rate=0.0005, device="auto")  # promising
@@ -110,10 +110,10 @@ def get_previous_model(env, custom_objects):
     return None, 0
 
 
-def train_last_model(total_time_steps=30_000, max_episode_steps=240*4, constant_cube_spawn=False, learning_rate=5e-4):
+def train_last_model(total_time_steps=30_000, ep_step_limit=240*4, constant_cube_spawn=False, learning_rate=5e-4):
     custom_objects = {'learning_rate': learning_rate}
     env = gym.make("robo_ml_gym:robo_ml_gym/RoboWorld-v0",
-                   max_episode_steps=max_episode_steps,
+                   ep_step_limit=ep_step_limit,
                    verbose=False,
                    total_steps_limit=total_time_steps,
                    constant_cube_spawn=constant_cube_spawn)
@@ -198,7 +198,7 @@ class Manager:
             lr = self.lrs_dict[model_name][self.lr_i]
             #PPO(n_steps=2048, batch_size=64, n_epochs=10)
 
-            env = gym.make(self.env_name, max_episode_steps=self.max_ep_steps, verbose=False,
+            env = gym.make(self.env_name, ep_step_limit=self.max_ep_steps, verbose=False,
                            total_steps_limit=self.total_time_steps_dict[model_name],
                            constant_cube_spawn=self.constant_cube_spawn)
             model = self.models_dict[model_name](self.policy_name, env, learning_rate=lr, verbose=1, device="auto",
@@ -212,7 +212,7 @@ class Manager:
         model_name = "SAC"
         if model_name in self.model_types_to_run:
             lr = self.lrs_dict[model_name][self.lr_i]
-            env = gym.make(self.env_name, max_episode_steps=self.max_ep_steps, verbose=True, total_steps_limit=self.total_time_steps_dict[model_name], constant_cube_spawn=self.constant_cube_spawn)
+            env = gym.make(self.env_name, ep_step_limit=self.max_ep_steps, verbose=True, total_steps_limit=self.total_time_steps_dict[model_name], constant_cube_spawn=self.constant_cube_spawn)
             model = self.models_dict[model_name](self.policy_name, env, learning_rate=lr, verbose=1, device="auto")
             # ensure info logs have the same name as the model
             env.unwrapped.set_fname(get_model_name(model, self.total_time_steps_dict[model_name]).strip("models/"))
@@ -223,7 +223,7 @@ class Manager:
         model_name = "A2C"
         if model_name in self.model_types_to_run:
             lr = self.lrs_dict[model_name][self.lr_i]
-            env = gym.make(self.env_name, max_episode_steps=self.max_ep_steps, verbose=True, total_steps_limit=self.total_time_steps_dict[model_name], constant_cube_spawn=self.constant_cube_spawn)
+            env = gym.make(self.env_name, ep_step_limit=self.max_ep_steps, verbose=True, total_steps_limit=self.total_time_steps_dict[model_name], constant_cube_spawn=self.constant_cube_spawn)
             model = self.models_dict[model_name](self.policy_name, env, learning_rate=lr, n_steps=5, verbose=1, device="auto")
             # ensure info logs have the same name as the model
             env.unwrapped.set_fname(get_model_name(model, self.total_time_steps_dict[model_name]).strip("models/"))
@@ -242,9 +242,9 @@ def newSAC():
     for es_lr in lrs:
         ep_steps, lr = es_lr
         for i in range(3):
-            train_last_model(total_time_steps=20_000, max_episode_steps=ep_steps, learning_rate=lr)
+            train_last_model(total_time_steps=20_000, ep_step_limit=ep_steps, learning_rate=lr)
     for i in range(100):
-        train_last_model(total_time_steps=20_000, max_episode_steps=240*8, learning_rate=1e-5)
+        train_last_model(total_time_steps=20_000, ep_step_limit=240*8, learning_rate=1e-5)
 
 
 if __name__ == '__main__':
@@ -258,25 +258,24 @@ if __name__ == '__main__':
     #newSAC()
 
     # run previously trained model
-    #train_last_model(total_time_steps=20_000, max_episode_steps=240)
+    #train_last_model(total_time_steps=20_000, ep_step_limit=240)
     #for i in range(20):
-    #    train_last_model(total_time_steps=20_000, max_episode_steps=240)
+    #    train_last_model(total_time_steps=20_000, ep_step_limit=240)
     #for i in range(10):
-    #    train_last_model(total_time_steps=20_000, max_episode_steps=240*2)
+    #    train_last_model(total_time_steps=20_000, ep_step_limit=240*2)
     #for i in range(3):
-    #    train_last_model(total_time_steps=20_000, max_episode_steps=240*4)
+    #    train_last_model(total_time_steps=20_000, ep_step_limit=240*4)
 
     #m = Manager(model_types_to_run=["PPO"], total_steps_limit=100_000, constant_cube_spawn=False, vary_learning_rates=False)
     #m.run()
     #time.sleep(60*15)
-    train_last_model(total_time_steps=20_000, max_episode_steps=240*6, learning_rate=2e-6)
-    train_last_model(total_time_steps=20_000, max_episode_steps=240*6, learning_rate=2e-6)
+    #train_last_model(total_time_steps=100_000, ep_step_limit=240*6, learning_rate=5e-6)
+    for i in range(20):
+        train_last_model(total_time_steps=100_000, ep_step_limit=240*6, learning_rate=5e-6)
     #for i in range(20):
-    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*4, learning_rate=3e-4)
+    #    train_last_model(total_time_steps=100_000, ep_step_limit=240*7, learning_rate=3e-5)
     #for i in range(20):
-    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*7, learning_rate=3e-5)
-    #for i in range(20):
-    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*7, learning_rate=2e-6)
+    #    train_last_model(total_time_steps=100_000, ep_step_limit=240*7, learning_rate=2e-6)
     #for i in range(4000):
-    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=1e-6)
+    #    train_last_model(total_time_steps=100_000, ep_step_limit=240*8, learning_rate=1e-6)
 
