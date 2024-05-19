36fa911432a6f4df375b5aeb2d83dc661ab0bf3e
Robin Gartz <76932159+robingartz@users.noreply.github.com>
2024-05-19 21:50:13 +0800

    A11

--- validation_train.py
+++ validation_train.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 from stable_baselines3 import PPO, SAC, A2C  # PPO, SAC, A2C, TD3, DDPG, HER-replay buffer
 import utils
 
-GROUP_PREFIX = "A10"
+GROUP_PREFIX = "A11"
 ENV_ROBOWORLD = "robo_ml_gym:robo_ml_gym/RoboWorld-v0"
 
 
@@ -37,16 +37,18 @@ def train_last_model(total_time_steps=100_000, max_episode_steps=240*8, constant
 
 if __name__ == '__main__':
     # probably worth having a few (8) points for the cube to spawn at and run those in batches?
-    train_new_ppo(total_steps_limit=10_000)
+    train_new_ppo(total_steps_limit=100_000)
     for i in range(20):
-        train_last_model(total_time_steps=10_000, max_episode_steps=240*4, learning_rate=3e-4)
-    for i in range(20):
-        train_last_model(total_time_steps=100_000, max_episode_steps=240*4, learning_rate=1e-4)
-    for i in range(20):
-        train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-5)
-    for i in range(20):
-        train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=1e-5)
-    for i in range(20):
-        train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-6)
-    for i in range(1000):
-        train_last_model(total_time_steps=100_000, max_episode_steps=240*12, learning_rate=1e-6)
+        train_last_model(total_time_steps=100_000, max_episode_steps=240*4, learning_rate=3e-4)
+    for i in range(40):
+        train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=3e-4)
+    #for i in range(20):
+    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*4, learning_rate=1e-4)
+    #for i in range(20):
+    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-5)
+    #for i in range(20):
+    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=1e-5)
+    #for i in range(20):
+    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-6)
+    #for i in range(1000):
+    #    train_last_model(total_time_steps=100_000, max_episode_steps=240*12, learning_rate=1e-6)
