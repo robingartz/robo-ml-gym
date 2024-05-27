254c1da135a689df41470fa8fb1f3df2da85ba74
Robin Gartz <76932159+robingartz@users.noreply.github.com>
2024-05-27 17:09:01 +0800

    Tidied

--- validation_train.py
+++ validation_train.py
@@ -50,17 +50,16 @@ if __name__ == '__main__':
     #    train_last_model(total_time_steps=100_000, max_episode_steps=240 * 6, learning_rate=3e-4)
     #model = "models/PPO-v9000k-A16-240526_064133"
 
-    train_new_ppo(total_steps_limit=100_000, ep_step_limit=240*8)
     for r in range(5):
+        train_new_ppo(total_steps_limit=100_000, ep_step_limit=240 * 8)
         train_last_model(total_time_steps=100_000, max_episode_steps=240 * 8, learning_rate=3e-4)
-        for i in range(80):
+        for i in range(200):
             train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=3e-4)
-        for i in range(30):
-            train_last_model(total_time_steps=100_000, max_episode_steps=240*12, learning_rate=3e-4)        #for i in range(20):
-        #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=1e-4)        #for i in range(20):
-        #    train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-5)        #for i in range(20):
-        #    train_last_model(total_time_steps=200_000, max_episode_steps=240*12, learning_rate=1e-5)
-        #for i in range(20):
-        #    train_last_model(total_time_steps=200_000, max_episode_steps=240*12, learning_rate=5e-6)
+        for i in range(10):
+            train_last_model(total_time_steps=100_000, max_episode_steps=240*12, learning_rate=3e-4)
+        #for i in range(20): train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=1e-4)
+        #for i in range(20): train_last_model(total_time_steps=100_000, max_episode_steps=240*8, learning_rate=5e-5)
+        #for i in range(20): train_last_model(total_time_steps=200_000, max_episode_steps=240*12, learning_rate=1e-5)
+        #for i in range(20): train_last_model(total_time_steps=200_000, max_episode_steps=240*12, learning_rate=5e-6)
     for i in range(200):
         train_last_model(total_time_steps=100_000, max_episode_steps=240*12, learning_rate=1e-5)
