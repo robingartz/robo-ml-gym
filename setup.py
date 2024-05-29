0e207e35d3f071afd6736e9080eeddd0bd610bae
Robin Gartz <76932159+robingartz@users.noreply.github.com>
2024-05-29 18:32:58 +0800

    Update requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="robo_ml_gym",
     version="0.0.1",
-    install_requires=["gymnasium", "pybullet", "numpy", "scipy"],
+    install_requires=["gymnasium", "pybullet", "numpy", "scipy", "wandb"],
 )
